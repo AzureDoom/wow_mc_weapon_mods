fc6626a5c8666f8b7d9fa26c4d188128b5217afa
AzureZhen <rcraft@cyberconnects2.com>
2020-02-21 01:46:00 -0500

    1.15.2

--- src/main/java/mod/azure/wowweapons/util/MMORPGHandler.java
+++ src/main/java/mod/azure/wowweapons/util/MMORPGHandler.java
@@ -1,36 +1,36 @@
 package mod.azure.wowweapons.util;
 
-//import com.robertx22.mine_and_slash.api.MineAndSlashAPI;
-//import com.robertx22.mine_and_slash.config.compatible_items.ConfigItem;
-//import com.robertx22.mine_and_slash.database.gearitemslots.weapons.Staff;
-//import com.robertx22.mine_and_slash.database.gearitemslots.weapons.Sword;
+import com.robertx22.mine_and_slash.api.MineAndSlashAPI;
+import com.robertx22.mine_and_slash.config.compatible_items.ConfigItem;
+import com.robertx22.mine_and_slash.database.gearitemslots.weapons.Staff;
+import com.robertx22.mine_and_slash.database.gearitemslots.weapons.Sword;
 
 import mod.azure.wowweapons.WoWWeaponsMod;
 
 public class MMORPGHandler {
 
 	public MMORPGHandler() {
-		//MineAndSlashAPI.addCompatibleItem(WoWWeaponsMod.MODID + ":marlis_touch",
-		//new ConfigItem().setType(Staff.INSTANCE).setSalvagable(true));
-		//MineAndSlashAPI.addCompatibleItem(WoWWeaponsMod.MODID + ":touch_of_chaos",
-		//new ConfigItem().setType(Staff.INSTANCE).setSalvagable(true));
-		//MineAndSlashAPI.addCompatibleItem(WoWWeaponsMod.MODID + ":doomfinger",
-		//new ConfigItem().setType(Staff.INSTANCE).setSalvagable(true));
-		//MineAndSlashAPI.addCompatibleItem(WoWWeaponsMod.MODID + ":anzusscorn",
-		//new ConfigItem().setType(Staff.INSTANCE).setSalvagable(true));
-		//MineAndSlashAPI.addCompatibleItem(WoWWeaponsMod.MODID + ":thoughtblighter",
-				//new ConfigItem().setType(Staff.INSTANCE).setSalvagable(true));
+		MineAndSlashAPI.addCompatibleItem(WoWWeaponsMod.MODID + ":marlis_touch",
+				new ConfigItem().setType(Staff.INSTANCE).setSalvagable(true));
+		MineAndSlashAPI.addCompatibleItem(WoWWeaponsMod.MODID + ":touch_of_chaos",
+				new ConfigItem().setType(Staff.INSTANCE).setSalvagable(true));
+		MineAndSlashAPI.addCompatibleItem(WoWWeaponsMod.MODID + ":doomfinger",
+				new ConfigItem().setType(Staff.INSTANCE).setSalvagable(true));
+		MineAndSlashAPI.addCompatibleItem(WoWWeaponsMod.MODID + ":anzusscorn",
+				new ConfigItem().setType(Staff.INSTANCE).setSalvagable(true));
+		MineAndSlashAPI.addCompatibleItem(WoWWeaponsMod.MODID + ":thoughtblighter",
+				new ConfigItem().setType(Staff.INSTANCE).setSalvagable(true));
 		WoWWeaponsMod.LOGGER.info("Registered Staffs");
-		//MineAndSlashAPI.addCompatibleItem(WoWWeaponsMod.MODID + ":thunderfury",
-		//new ConfigItem().setType(Sword.INSTANCE).setSalvagable(true));
-		//MineAndSlashAPI.addCompatibleItem(WoWWeaponsMod.MODID + ":armageddon",
-		//new ConfigItem().setType(Sword.INSTANCE).setSalvagable(true));
-		//MineAndSlashAPI.addCompatibleItem(WoWWeaponsMod.MODID + ":frostmourne",
-		//new ConfigItem().setType(Sword.INSTANCE).setSalvagable(true));
-		//MineAndSlashAPI.addCompatibleItem(WoWWeaponsMod.MODID + ":ashbringer",
-		//new ConfigItem().setType(Sword.INSTANCE).setSalvagable(true));
-		//MineAndSlashAPI.addCompatibleItem(WoWWeaponsMod.MODID + ":swordofathousandtruths",
-		//new ConfigItem().setType(Sword.INSTANCE).setSalvagable(true));
+		MineAndSlashAPI.addCompatibleItem(WoWWeaponsMod.MODID + ":thunderfury",
+				new ConfigItem().setType(Sword.INSTANCE).setSalvagable(true));
+		MineAndSlashAPI.addCompatibleItem(WoWWeaponsMod.MODID + ":armageddon",
+				new ConfigItem().setType(Sword.INSTANCE).setSalvagable(true));
+		MineAndSlashAPI.addCompatibleItem(WoWWeaponsMod.MODID + ":frostmourne",
+				new ConfigItem().setType(Sword.INSTANCE).setSalvagable(true));
+		MineAndSlashAPI.addCompatibleItem(WoWWeaponsMod.MODID + ":ashbringer",
+				new ConfigItem().setType(Sword.INSTANCE).setSalvagable(true));
+		MineAndSlashAPI.addCompatibleItem(WoWWeaponsMod.MODID + ":swordofathousandtruths",
+				new ConfigItem().setType(Sword.INSTANCE).setSalvagable(true));
 		WoWWeaponsMod.LOGGER.info("Registered Swords");
 	}
 }
