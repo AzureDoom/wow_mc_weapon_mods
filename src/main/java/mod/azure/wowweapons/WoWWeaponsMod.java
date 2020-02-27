d7b022eb3330b8b54fcd2663a57f6c5d36286214
AzureZhen <rcraft@cyberconnects2.com>
2020-02-27 08:24:10 -0500

    v1.3.0

--- src/main/java/mod/azure/wowweapons/WoWWeaponsMod.java
+++ src/main/java/mod/azure/wowweapons/WoWWeaponsMod.java
@@ -6,14 +6,16 @@ import org.apache.logging.log4j.Logger;
 import mod.azure.wowweapons.config.Config;
 import mod.azure.wowweapons.util.LootHandler;
 import mod.azure.wowweapons.util.MMORPGHandler;
+import net.minecraft.data.DataGenerator;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
+import net.minecraftforge.fml.event.lifecycle.GatherDataEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.loading.FMLPaths;
 
@@ -29,7 +31,6 @@ public class WoWWeaponsMod {
 		ModLoadingContext modLoadingContext = ModLoadingContext.get();
 		IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 		modEventBus.addListener(this::setup);
-		modEventBus.addListener(this::enqueueIMC);
 		modLoadingContext.registerConfig(ModConfig.Type.SERVER, Config.SERVER_SPEC, "wowweapons-config.toml");
 		Config.loadConfig(Config.SERVER_SPEC,
 				FMLPaths.CONFIGDIR.get().resolve("wowweapons-config.toml").toString());
@@ -41,9 +42,16 @@ public class WoWWeaponsMod {
 		}
 	}
 
-	private void enqueueIMC(final InterModProcessEvent event) {
-		if (ModList.get().isLoaded("mmorpg") && Config.SERVER.USE_COMPATIBILITY_ON_ITEMS.get()) {
-			MinecraftForge.EVENT_BUS.register(new MMORPGHandler());
+	@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+	public static class GatherDataSubscriber {
+		@SubscribeEvent
+		public static void gatherData(GatherDataEvent event) {
+			DataGenerator gen = event.getGenerator();
+			if (event.includeServer()) {
+				if (ModList.get().isLoaded("mmorpg") && Config.SERVER.USE_COMPATIBILITY_ON_ITEMS.get()) {
+					gen.addProvider(new MMORPGHandler().getDataPackCreator(gen));
+				}
+			}
 		}
 	}
 }
